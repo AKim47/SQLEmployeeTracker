43c237c707ac0eb60461cc2bff66646de587191f
AKim47 <abel.kim.22@gmail.com>
2022-02-08 17:32:03 -0600

    finish 'Adding Department, Role, and Employee'

--- db/schema.sql
+++ db/schema.sql
@@ -4,24 +4,24 @@ DROP TABLE IF EXISTS roles;
 DROP TABLE IF EXISTS employee;
 
 CREATE TABLE department (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    department_id INTEGER AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE roles (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    role_id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INTEGER,
-    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(department_id) ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    employee_id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR (30) NOT NULL,
     role_id INTEGER,
     manager_id INTEGER,
-    CONSTRAINT fk_roles FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
-    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+    CONSTRAINT fk_roles FOREIGN KEY (role_id) REFERENCES roles(role_id) ON DELETE SET NULL,
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(employee_id) ON DELETE SET NULL
 );
--- index.js
+++ index.js
@@ -28,10 +28,10 @@ const initialPrompt = staging => {
             return addDepartment();
         }
         else if (staging.options === 'Add a Role') {
-            // return addRole
+            return addRole();
         }
         else if (staging.options === 'Add an Employee') {
-            // return addEmployee
+            return addEmployee();
         }
         else if (staging.options === 'Update an Employee') {
             // return updateEmployee
@@ -44,19 +44,19 @@ async function viewDepartment() {
     const [rows, data] = await db.query(`SELECT * FROM department`);
     console.table(rows);
     initialPrompt();
-}
+};
 
 async function viewRole() {
-    const [rows, data] = await db.query(`SELECT * FROM roles`);
+    const [rows, data] = await db.query(`SELECT * FROM roles LEFT JOIN department ON roles.department_id = department.department_id`);
     console.table(rows);
     initialPrompt();
-}
+};
 
 async function viewEmployee() {
-    const [rows, data] = await db.query(`SELECT * FROM employee`);
+    const [rows, data] = await db.query(`SELECT * FROM employee LEFT JOIN roles ON employee.role_id = roles.role_id LEFT JOIN department ON roles.department_id = department.department_id`);
     console.table(rows);
     initialPrompt();
-}
+};
 
 const addDepartment = dept => {
     dept = [];
@@ -64,26 +64,123 @@ const addDepartment = dept => {
         {
             type: 'input',
             name: 'Dept',
-            message: 'What is the name of the Department',
-            validate: deptInput => {
-                if(deptInput) {
-                    return true;
-                } else {
-                    console.log('Please enter the Department name');
-                    return false;
-                }
-            }
+            message: 'What is the name of the Department'
         }
     ])
     .then(deptData => {
         return queryDepartment(deptData['Dept']);
     });
-}
+};
+
+const addRole = role => {
+    role = [];
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'Title',
+            message: 'What is the title of the Role',
+            validate: titleInput => {
+                if(titleInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the Role title');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'number',
+            name: 'Salary',
+            message: 'What is the salary of the Role',
+            validate: salaryInput => {
+                if(salaryInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the Role salary');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'number',
+            name: 'Department',
+            message: 'What is the Department id of the Role'
+
+        }
+    ])
+    .then(roleData => {
+        return queryRoles(roleData['Title'], roleData['Salary'], roleData['Department']);
+    });
+};
+
+const addEmployee = employee => {
+    employee = [];
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'first_name',
+            message: 'What is the first_name'
+        },
+        {
+            type: 'input',
+            name: 'last_name',
+            message: 'What is the last_name'
+        },
+        {
+            type: 'number',
+            name: 'Role',
+            message: 'What is the Role id'
+
+        },
+        {
+            type: 'number',
+            name: 'Manager',
+            message: 'What is the Manager id'
+
+        }
+    ])
+    .then(employeeData => {
+        return queryEmployee(employeeData['first_name'], employeeData['last_name'], employeeData['Role'], employeeData['Manager']);
+    });
+};
 
 async function queryDepartment(name) {
-    const sql = `INSERT INTO department (name) VALUES (?)`;
-    const params = await [name];
-    const run = await db.query(sql, params);
-}
+    const sql = `INSERT INTO department name VALUES ("${name}")`;
+    const params = [name];
+    await db.query(sql, params, (err, result) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+    });
+    initialPrompt();
 
-initialPrompt()
+};
+
+async function queryRoles(title, salary, department_id) {
+    const sql = `INSERT INTO roles (title, salary, department_id) VALUES ("${title}", ${salary}, ${department_id})`;
+    const params = [title, salary, department_id];
+    await db.query(sql, params, (err, result) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+    });
+    initialPrompt();
+
+};
+
+async function queryEmployee(first_name, last_name, role_id, manager_id) {
+    const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("${first_name}", "${last_name}", ${role_id}, ${manager_id})`;
+    const params = [first_name, last_name, role_id, manager_id];
+    await db.query(sql, params, (err, result) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+    });
+
+    initialPrompt();
+};
+
+initialPrompt();
